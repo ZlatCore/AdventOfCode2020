a382e3630bbc16b1e2f177a8d83c2f7e31052e1a
Zlatko Radojcic <zlatko.rad.alt@gmail.com>
2020-12-02 10:31:44 +0100

    Task 1 solution

---
+++ dec-02/dec02task1/PasswordData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dec02task1
+{
+    public class PasswordData
+    {
+        public int LowerLimit { get; private set; }
+        public int UpperLimit { get; private set; }
+        public char RequiredChar { get; private set; }
+        public string Password { get; private set; }
+
+        public bool IsValid { get; private set; }
+
+        public PasswordData(string dataString)
+        {
+            // 1-3 a: abcde
+            var tmp = dataString.Split(" ");
+            this.Password = tmp[2];
+            this.RequiredChar = tmp[1][0];
+            var limits = tmp[0].Split("-");
+            this.LowerLimit = int.Parse(limits[0]);
+            this.UpperLimit = int.Parse(limits[1]);
+
+            var tmpCount = 0;
+            foreach (var s in this.Password)
+            {
+                if (s == this.RequiredChar)
+                {
+                    tmpCount++;
+                }
+            }
+
+            this.IsValid = tmpCount >= this.LowerLimit && tmpCount <= this.UpperLimit;
+        }
+    }
+}
---
+++ dec-02/dec02task1/TaskSolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dec02task1
+{
+    public class TaskSolver
+    {
+
+        List<string> input;
+        List<PasswordData> convertedInput;
+
+        public TaskSolver(List<string> input)
+        {
+            this.input = input;
+            this.convertedInput = this.input.Select(x => new PasswordData(x)).ToList();
+        }
+
+        public int GetValidPasswordsNumber()
+        {
+            return this.convertedInput.Count(x => x.IsValid);
+        }
+    }
+}
