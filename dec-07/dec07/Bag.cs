fb13cb370929aad2f9e6064cef478e381477b912
Zlatko Radojcic <zlatko.rad.alt@gmail.com>
2020-12-07 07:52:21 +0100

    Day 7 solutions

---
+++ dec-07/dec07/Bag.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dec07
+{
+    public class Bag
+    {
+        public string Name { get; set; }
+        public List<ContainedBag> Contains { get; set; }
+
+        public Bag(string input)
+        {
+            this.Contains = new List<ContainedBag>();
+            var splitInput = input.Split(" contain ");
+            this.Name = splitInput[0];
+            this.Name = this.Name.Replace("bags", "bag");
+            var containsString = splitInput[1].TrimEnd('.');
+            if (containsString.StartsWith("no"))
+            {
+                return;
+            }
+            var contains = containsString.Split(", ");
+            foreach (var cb in contains)
+            {
+                var tmp = cb.Split(new char[] { ' ' }, 2);
+                int count = int.Parse(tmp[0]);
+                var name = tmp[1];
+                name = name.Replace("bags", "bag");
+                this.Contains.Add(new ContainedBag() { Name = name, Count = count });
+            }
+
+        }
+    }
+
+    public class ContainedBag
+    {
+        public int Count { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ dec-07/dec07/TaskSolver.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dec07
+{
+    public class TaskSolver
+    {
+        string[] originalInput;
+        List<Bag> BagList;
+        public TaskSolver(string[] input)
+        {
+            this.originalInput = input;
+            this.BagList = input.Select(x => new Bag(x)).ToList();
+        }
+
+        public int CountCanContain(string bagName)
+        {
+            var alreadyChecked = new List<string>();
+            var toCheckNext = new Queue<string>();
+
+            var directHolders = this.GetDirectHolderFor(bagName);
+
+            foreach (var bag in directHolders)
+            {
+                toCheckNext.Enqueue(bag);
+            }
+
+            while (toCheckNext.Count > 0)
+            {
+                var bagToCheck = toCheckNext.Dequeue();
+                if (alreadyChecked.Contains(bagToCheck))
+                {
+                    continue;
+                }
+
+                alreadyChecked.Add(bagToCheck);
+
+                var indirectHolders = this.GetDirectHolderFor(bagToCheck);
+                foreach (var ih in indirectHolders)
+                {
+                    toCheckNext.Enqueue(ih);
+                }
+            }
+
+            return alreadyChecked.Count();
+        }
+
+        public int CountBagsInside(string bagToCheck)
+        {
+            int result = 1;
+            var bag = this.BagList.Find(x => x.Name == bagToCheck);
+            if (bag != null)
+            {
+                foreach (var cb in bag.Contains)
+                {
+                    result += cb.Count * this.CountBagsInside(cb.Name);
+                }
+            }
+
+            return result;
+        }
+
+        private List<string> GetDirectHolderFor(string bagName)
+        {
+            return this.BagList.Where(x => x.Contains.Any(c => c.Name == bagName)).Select(x => x.Name).ToList();
+        }
+    }
+}
