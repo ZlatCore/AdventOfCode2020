923dc9773010f85543229d01ce5053be92c0a78a
Zlatko Radojcic <zlatko.rad.alt@gmail.com>
2020-12-08 07:55:15 +0100

    Solutions for day 8, could be improved but why

---
+++ dec-08/day08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace day08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ts = new TaskSolver(TaskInput.GetInput());
+
+            var result = ts.FindAccumulatorOnSecondExecution();
+
+            Console.WriteLine($"result is: {result}");
+
+            var result2 = ts.FindFixedAccumulator();
+            Console.WriteLine($"result2 is: {result2}");
+        }
+    }
+}
---
+++ dec-08/day08/TaskSolver.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace day08
+{
+    class TaskSolver
+    {
+        private string[] originalInput;
+
+        public TaskSolver(string[] input)
+        {
+            this.originalInput = input;
+        }
+
+        public int FindAccumulatorOnSecondExecution()
+        {
+            int acc = 0;
+            int i = 0;
+            List<int> visitedLines = new List<int>();
+            bool lineWouldRepeat = false;
+            
+            while (!lineWouldRepeat)
+            {
+                var tmp = this.originalInput[i].Split(" ");
+                visitedLines.Add(i);
+                if (tmp[0] == "acc")
+                {
+                    int change = int.Parse(tmp[1]);
+                    acc += change;
+                    i++;
+                }
+                else if (tmp[0] == "jmp")
+                {
+                    int jump = int.Parse(tmp[1]);
+                    i += jump;
+                }
+                else
+                {
+                    i++;
+                }
+
+                lineWouldRepeat = visitedLines.Contains(i);
+            }
+
+            return acc;
+        }
+
+        public int FindFixedAccumulator()
+        {
+            //var result = this.TestChange(0, new int[] { }, 0);
+            //return result.Item2;
+            List<int> localVisited = new List<int>();
+            var i = 0;
+            bool lineWouldRepeat = localVisited.Contains(i);
+            var acc = 0;
+            while (!lineWouldRepeat)
+            {
+                var tmp = this.originalInput[i].Split(" ");
+                localVisited.Add(i);
+                if (tmp[0] == "acc")
+                {
+                    int change = int.Parse(tmp[1]);
+                    acc += change;
+                    i++;
+                }
+                else if (tmp[0] == "jmp")
+                {
+                    var iToTest = i + 1;
+                    if (!localVisited.Contains(iToTest))
+                    {
+                        var testJumpToNop = this.TestChange(iToTest, localVisited.ToArray(), acc);
+                        if (testJumpToNop.Item1)
+                        {
+                            return testJumpToNop.Item2;
+                        }
+
+                    }
+
+                    int jump = int.Parse(tmp[1]);
+                    i += jump;
+                }
+                else
+                {
+                    var iToTest = i + int.Parse(tmp[1]);
+                    if (!localVisited.Contains(iToTest))
+                    {
+                        var testNopToJump = this.TestChange(iToTest, localVisited.ToArray(), acc);
+                        if (testNopToJump.Item1)
+                        {
+                            return testNopToJump.Item2;
+                        }
+                    }
+
+                    i++;
+                }
+
+                lineWouldRepeat = localVisited.Contains(i);
+            }
+
+            return acc;
+        }
+
+        private (bool, int) TestChange(int newI, int[] visited, int currentAcc)
+        {
+            List<int> localVisited = visited.ToList();
+            var i = newI;
+            bool lineWouldRepeat = localVisited.Contains(i);
+            var acc = currentAcc;
+            while (!lineWouldRepeat)
+            {
+                var tmp = this.originalInput[i].Split(" ");
+                localVisited.Add(i);
+                if (tmp[0] == "acc")
+                {
+                    int change = int.Parse(tmp[1]);
+                    acc += change;
+                    i++;
+                }
+                else if (tmp[0] == "jmp")
+                {
+                    int jump = int.Parse(tmp[1]);
+                    i += jump;
+                }
+                else
+                {
+                    i++;
+                }
+
+                if (i == this.originalInput.Length)
+                {
+                    return (true, acc);
+                }
+
+                lineWouldRepeat = localVisited.Contains(i);
+            }
+
+            return (false, 0);
+        }
+    }
+}
